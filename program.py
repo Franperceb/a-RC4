8f7c1bdcb152933be30a4c1b093747325d4cd79d
JorgeCeb <franperceb@gmail.com>
2022-03-09 17:20:27 -0600

    RC4 completed

---
+++ program.py
@@ -0,0 +1,59 @@
+import fileinput
+
+#2 digits hexadecimal
+def ToHexadecimal(n): 
+	hexadecimal = hex(n).split('x')[1]
+	if len(h) < 2:
+		hexadecimal = "0" + h
+	return hexadecimal.upper()
+
+#Key Scheduling Algorithm.
+def KSA(k): 
+	key = [ord(c) for c in key]
+  permutation = [] #S
+  j = 0
+
+	for i in range(0,256):
+		permutation.append(i)
+
+	for i in range(0,256):
+		j = (j + permutation[i] + key[i % len(k)]) % 256
+   #Swapping values 
+		aux = permutation[i] 
+		permutation[i] = permutation[j]
+		permutation[j] = aux
+	return permutation
+
+#Pseudorandom generation algorithm
+def PRGA(S,size): 
+	i  = 0
+  j = 0
+	for s in range (0,size):
+		i = (i + 1) % 256
+		j = (j + S[i]) % 256
+    #swapping values
+		aux = S[i]
+		S[i] = S[j]
+		S[j] = aux
+		KeyEncrypted = S[(S[i] + S[j]) % 256]
+		yield KeyEncrypted
+
+def rc4(key,msg):
+	encryptedMsg = ''
+  S=KSA(key)
+  counter = 0 #to get every character of the text.
+	keyStream = PRGA(S,len(msg))
+	for i in keyStream:
+		r = r + ToHexadecimal(ord(msg[c])^i)
+		counter += 1
+	return encryptedMsg
+
+#Alphagrader testing
+lines = []
+for line in fileinput.input():
+    line = line.replace('\n','')
+    lines.append(line)
+    if(len(lines) == 2):
+        print(RC4(lines[0],lines[1]))
+
+
